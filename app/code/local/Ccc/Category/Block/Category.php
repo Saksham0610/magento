126464173fd8323ff6b024937614519604e82421
Saksham0610 <sakshamjaswal01@gmail.com>
2023-05-09 11:58:52 +0530

    category-module

---
+++ app/code/local/Ccc/Category/Block/Category.php
@@ -0,0 +1,8 @@
+<?php
+class Ccc_Category_Block_Category extends Mage_Core_Block_Template
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
---
+++ app/code/local/Ccc/Category/controllers/IndexController.php
@@ -0,0 +1,8 @@
+<?php
+class Ccc_Category_IndexController extends Mage_Core_Controller_Front_Action
+{
+    function indexAction()
+    {
+        echo 111;
+    }
+}
