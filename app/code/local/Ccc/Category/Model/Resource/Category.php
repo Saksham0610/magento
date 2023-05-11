47a3b9853c09ae2055921646bfc691ac3dcf0bd3
Saksham0610 <sakshamjaswal01@gmail.com>
2023-05-11 15:58:48 +0530

    product-module

--- app/code/local/Ccc/Category/Model/Resource/Category.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Ccc_Category_Model_Resource_Category extends Mage_Core_Model_Resource_Db_Abstract
-{
-    function _construct()
-    {
-        $this->_init('category/category', 'category_id');
-    }
-}
